c0165c06e69bb5eda62796bd966e9cb074d2e4ff
ven-gnz <kangas.otso@gmail.com>
2025-11-07 13:49:58 +0200

    Some inertia calculations

--- Impi/src/physics/RigidBody.cpp
+++ Impi/src/physics/RigidBody.cpp
@@ -7,9 +7,11 @@ using namespace Impi;
 
 
 // https://stackoverflow.com/questions/3992980/c-inline-member-function-in-cpp-file
-/*
-
-*/
+/**
+ * Inline function that creates a transform matrix from a position and orientation.
+  Note that the implementation of this function was created by an
+  automated code-generator and optimizer.
+ */
 static inline void _calculateTransformMatrix(Matrix4& transformMatrix,
     const Vector3& position,
     const Quaternion& orientation)
@@ -37,6 +39,73 @@ static inline void _calculateTransformMatrix(Matrix4& transformMatrix,
     transformMatrix.data[10] = 1 - 2 * orientation.i * orientation.i -
         2 * orientation.j * orientation.j;
     transformMatrix.data[11] = position.z;
+}
+
+/**
+  Internal function to do an intertia tensor transform by a quaternion.
+  Note that the implementation of this function was created by an
+  automated code-generator and optimizer.
+ */
+static inline void _transformInertiaTensor(Matrix3& iitWorld,
+    const Quaternion& q,
+    const Matrix3& iitBody,
+    const Matrix4& rotmat)
+{
+    real t4 = rotmat.data[0] * iitBody.data[0] +
+        rotmat.data[1] * iitBody.data[3] +
+        rotmat.data[2] * iitBody.data[6];
+    real t9 = rotmat.data[0] * iitBody.data[1] +
+        rotmat.data[1] * iitBody.data[4] +
+        rotmat.data[2] * iitBody.data[7];
+    real t14 = rotmat.data[0] * iitBody.data[2] +
+        rotmat.data[1] * iitBody.data[5] +
+        rotmat.data[2] * iitBody.data[8];
+    real t28 = rotmat.data[4] * iitBody.data[0] +
+        rotmat.data[5] * iitBody.data[3] +
+        rotmat.data[6] * iitBody.data[6];
+    real t33 = rotmat.data[4] * iitBody.data[1] +
+        rotmat.data[5] * iitBody.data[4] +
+        rotmat.data[6] * iitBody.data[7];
+    real t38 = rotmat.data[4] * iitBody.data[2] +
+        rotmat.data[5] * iitBody.data[5] +
+        rotmat.data[6] * iitBody.data[8];
+    real t52 = rotmat.data[8] * iitBody.data[0] +
+        rotmat.data[9] * iitBody.data[3] +
+        rotmat.data[10] * iitBody.data[6];
+    real t57 = rotmat.data[8] * iitBody.data[1] +
+        rotmat.data[9] * iitBody.data[4] +
+        rotmat.data[10] * iitBody.data[7];
+    real t62 = rotmat.data[8] * iitBody.data[2] +
+        rotmat.data[9] * iitBody.data[5] +
+        rotmat.data[10] * iitBody.data[8];
+
+    iitWorld.data[0] = t4 * rotmat.data[0] +
+        t9 * rotmat.data[1] +
+        t14 * rotmat.data[2];
+    iitWorld.data[1] = t4 * rotmat.data[4] +
+        t9 * rotmat.data[5] +
+        t14 * rotmat.data[6];
+    iitWorld.data[2] = t4 * rotmat.data[8] +
+        t9 * rotmat.data[9] +
+        t14 * rotmat.data[10];
+    iitWorld.data[3] = t28 * rotmat.data[0] +
+        t33 * rotmat.data[1] +
+        t38 * rotmat.data[2];
+    iitWorld.data[4] = t28 * rotmat.data[4] +
+        t33 * rotmat.data[5] +
+        t38 * rotmat.data[6];
+    iitWorld.data[5] = t28 * rotmat.data[8] +
+        t33 * rotmat.data[9] +
+        t38 * rotmat.data[10];
+    iitWorld.data[6] = t52 * rotmat.data[0] +
+        t57 * rotmat.data[1] +
+        t62 * rotmat.data[2];
+    iitWorld.data[7] = t52 * rotmat.data[4] +
+        t57 * rotmat.data[5] +
+        t62 * rotmat.data[6];
+    iitWorld.data[8] = t52 * rotmat.data[8] +
+        t57 * rotmat.data[9] +
+        t62 * rotmat.data[10];
 }
 
 
