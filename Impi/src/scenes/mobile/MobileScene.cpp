bea06ab319afb360c925a94ea6920b0895c24495
ven-gnz <kangas.otso@gmail.com>
2025-11-12 18:16:33 +0200

    Removed redundant operations around update on mobile scene

--- Impi/src/scenes/mobile/MobileScene.cpp
+++ Impi/src/scenes/mobile/MobileScene.cpp
@@ -9,32 +9,52 @@ MobileScene::MobileScene(Camera& camera)
         "src/scenes/mobile/shaders/mobile.vert",
         "src/scenes/mobile/shaders/mobile.frag",
         nullptr),
-        centerPoint(Vector3(0,0,0)),
-        mobile1_initialPos(Vector3(-2,-1,0)),
+        centerPoint(Vector3(0,10,0)),
+        mobile1_initialPos(Vector3(-4,4,0)),
         mobile1_offset(1,0,0),
         center_to_1offset(-1,0,0),
-        mobile2_initialPos(Vector3(2,-1,0)),
+        mobile2_initialPos(Vector3(4,4,0)),
         mobile2_offset(1,0,0),
         center_to_2offset(-1,0,0),
         defaultSpringConstant(15.5f),
-        defaultRestLength(2),
+        defaultRestLength(6),
         restLength1(defaultRestLength),
         restLength2(defaultRestLength),
-        spring1(center_to_1offset,&attachment1,mobile1_offset,defaultSpringConstant,defaultRestLength),
-        spring2(center_to_2offset,&attachment2,mobile2_offset,defaultSpringConstant,defaultRestLength)
+        centerpiece(),
+        attachment1(),
+        attachment2(),
+        spring1(center_to_1offset,&centerpiece,mobile1_offset,defaultSpringConstant,defaultRestLength),
+        spring2(center_to_2offset,&centerpiece,mobile2_offset,defaultSpringConstant,defaultRestLength),
+        registry(),
+        scene_gravity(Gravity())
 
 {
+    centerpiece.setOrientation(Quaternion(1, 0, 0, 0));
     centerpiece.setPosition(centerPoint);
+    centerpiece.calculateDerivedData();
+
+    attachment1.setOrientation(Quaternion(1, 0, 0, 0));
     attachment1.setPosition(mobile1_initialPos);
+    attachment1.calculateDerivedData(); 
+
+    attachment2.setOrientation(Quaternion(1, 0, 0, 0));
     attachment2.setPosition(mobile2_initialPos);
+    attachment2.calculateDerivedData();
+
 
     sphere_mesh.createMesh(1.0f);
     sphere_mesh.uploadToGPU();
     spheremesh_ptr = &sphere_mesh;
 
-    renderables.push_back(RenderableRigidBody(&centerpiece, spheremesh_ptr, 2.1));
-    renderables.push_back(RenderableRigidBody(&attachment1, spheremesh_ptr, 0.8));
-    renderables.push_back(RenderableRigidBody(&attachment2, spheremesh_ptr, 0.8));
+    registry.add(&attachment1, &spring1);
+    registry.add(&attachment1, &scene_gravity);
+    registry.add(&attachment2, &spring2);
+    registry.add(&attachment2, &scene_gravity);
+
+
+    renderables.push_back(RenderableRigidBody(&centerpiece, spheremesh_ptr, 1.0));
+    renderables.push_back(RenderableRigidBody(&attachment1, spheremesh_ptr, 1.0));
+    renderables.push_back(RenderableRigidBody(&attachment2, spheremesh_ptr, 1.0));
 }
 
 void MobileScene::draw(Renderer& renderer, Camera& camera)
@@ -45,17 +65,12 @@ void MobileScene::draw(Renderer& renderer, Camera& camera)
     glBindVertexArray(groundmesh_ptr->vao);
     groundmesh_ptr->draw();
 
-    for (auto& renderable : renderables)
-    {
-        renderable.updateModelMatrix();
-    }
-
     shader.use();
-
-
+   
     glBindVertexArray(spheremesh_ptr->vao);
     for (auto& r : renderables)
     {
+        r.updateModelMatrix();
         shader.setMat4("model", r.model);
         r.mesh->draw();
     }
@@ -72,25 +87,47 @@ void MobileScene::update(real dt)
     std::cout << "centerpiece r" << orientation.k << std::endl;
     std::cout << "centerpiece r" << orientation.r << std::endl;
 
+    Vector3 pos = centerpiece.getPosition();
+    std::cout << "Position: x=" << pos.x
+        << " y=" << pos.y
+        << " z=" << pos.z << std::endl;
 
-    centerpiece.clearAccumulators();
-    attachment1.clearAccumulators();
-    attachment2.clearAccumulators();
+    Vector3 pos1 = attachment1.getPosition();
+    Vector3 pos2 = attachment2.getPosition();
+
+    std::cout << "--- Attachment1 --- x=" << pos1.x
+        << " y=" << pos1.y
+        << " z=" << pos1.z << std::endl;
+
+    std::cout << "--- Attachment2 --- x=" << pos2.x
+        << " y=" << pos2.y
+        << " z=" << pos2.z << std::endl;
 
     registry.updateForces(dt);
 
     centerpiece.integrate(dt);
     attachment1.integrate(dt);
     attachment2.integrate(dt);
+
+    //Vector3 vel = centerpiece.getVelocity();
+    //Vector3 angVel = centerpiece.getAngularVelocity();
+    //std::cout << "Linear velocity: x=" << vel.x << " y=" << vel.y << " z=" << vel.z << std::endl;
+    //std::cout << "Angular velocity: x=" << angVel.x << " y=" << angVel.y << " z=" << angVel.z << std::endl;
+
 }
 
 void MobileScene::onActivate()
 {
     centerpiece.setOrientation(Quaternion(1, 0, 0, 0));
     centerpiece.setPosition(centerPoint);
+    centerpiece.calculateDerivedData();
     attachment1.setPosition(mobile1_initialPos);
+    attachment1.calculateDerivedData();
     attachment2.setPosition(mobile2_initialPos);
+    attachment2.calculateDerivedData();
     centerpiece.addTorque(Vector3(0, 2, 0));
+    centerpiece.calculateDerivedData();
+    camera.Position = camera.defaultPos+ glm::vec3(0, 0, 12);
 }
 
 
