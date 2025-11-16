3af0c714f6e67c3841d1f3280ef444aa343c8d41
ven-gnz <kangas.otso@gmail.com>
2025-11-16 19:44:25 +0200

    Mobile scene fixed with better sensible defaults

--- Impi/src/scenes/mobile/MobileScene.cpp
+++ Impi/src/scenes/mobile/MobileScene.cpp
@@ -12,7 +12,7 @@ MobileScene::MobileScene(Camera& camera)
         centerPoint(Vector3(0,7,0)),
         mobile1_initialPos(Vector3(-4,2,0)),
         mobile2_initialPos(Vector3(4,2,0)),
-        defaultSpringConstant(10.0f),
+        defaultSpringConstant(55.0f),
         defaultRestLength(5),
         restLength1(defaultRestLength),
         restLength2(defaultRestLength),
@@ -38,7 +38,7 @@ MobileScene::MobileScene(Camera& camera)
     attachment1.setOrientation(Quaternion(1, 0, 0, 0));
     attachment1.setPosition(mobile1_initialPos);
     attachment1.setInertiaTensor(i);
-    attachment1.setMass(25.0f);
+    attachment1.setMass(1.0f);
     attachment1.setLinearDamping(0.1f);
     attachment1.setAngularDamping(0.1f);
     attachment1.calculateDerivedData();
@@ -46,7 +46,7 @@ MobileScene::MobileScene(Camera& camera)
     attachment2.setOrientation(Quaternion(1, 0, 0, 0));
     attachment2.setPosition(mobile2_initialPos);
     attachment2.setInertiaTensor(i);
-    attachment2.setMass(25.0f);
+    attachment2.setMass(1.0f);
     attachment2.setLinearDamping(0.1f);
     attachment2.setAngularDamping(0.1f);
     attachment2.calculateDerivedData();
@@ -65,16 +65,10 @@ MobileScene::MobileScene(Camera& camera)
     spring1 = new Spring(zero, &centerpiece, o1, defaultSpringConstant, restLength1);
     spring2 = new Spring(zero, &centerpiece, o2, defaultSpringConstant, restLength2);
 
-
-
-    motor.setTorque(Vector3(0, 0.25, 0));
-
     registry.add(&attachment1, spring1);
     registry.add(&attachment2, spring2);
 
-    registry.add(&centerpiece, spring1);
-    registry.add(&centerpiece, spring2);
-
+    motor.setTorque(Vector3(0, 0.75, 0));
     registry.add(&attachment1, &scene_gravity);
     registry.add(&attachment2, &scene_gravity);
     registry.add(&centerpiece, &scene_gravity);
@@ -99,7 +93,6 @@ void MobileScene::draw(Renderer& renderer, Camera& camera)
     glBindVertexArray(groundmesh_ptr->vao);
     groundmesh_ptr->draw();
 
-
     shader.use();
     shader.setVec3("color", glm::vec3(0.9, 0.9, 0.9));
     glBindVertexArray(spheremesh_ptr->vao);
@@ -116,7 +109,7 @@ void MobileScene::draw(Renderer& renderer, Camera& camera)
     lineShader.use();
     
     Vector3 a1 = spring1->getAnchorWorldB();
-    Vector3 b1 = spring1->getAnchorWorldA(&attachment1);           // attachment 
+    Vector3 b1 = spring1->getAnchorWorldA(&attachment1);           
 
     att1.setPoints(
         glm::vec3(a1.x, a1.y, a1.z),
@@ -126,7 +119,7 @@ void MobileScene::draw(Renderer& renderer, Camera& camera)
     att1.draw();
     
     Vector3 a2 = spring2->getAnchorWorldB();
-    Vector3 b2 = spring2->getAnchorWorldA(&attachment2);             // attachment
+    Vector3 b2 = spring2->getAnchorWorldA(&attachment2);
 
     att2.setPoints(
         glm::vec3(a2.x, a2.y, a2.z),
@@ -141,9 +134,8 @@ void MobileScene::draw(Renderer& renderer, Camera& camera)
 
 void MobileScene::update(real dt) 
 {
-
+    
     registry.updateForces(dt);
-
     centerpiece.integrate(dt);
     attachment1.integrate(dt);
     attachment2.integrate(dt);
